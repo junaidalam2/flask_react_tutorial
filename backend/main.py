9ba0bcaca5c4944a80f1e6dd8d6dc8f01a80d4c7
junaidalam2 <junaid.alam2@gmail.com>
2025-10-11 11:47:38 -0700

    completed routes and requests

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,69 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import request, jsonify
+from config import app, db
+from models import Contact
 
-app = Flask(__name__)
-CORS(app)  # allow React frontend to call API
 
-@app.route("/api/hello")
-def hello():
-    return jsonify({"message": "Hello from Flask!"})
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all() # uses flask SQLAlchemy to obtain all records
+    json_contacts = list(map(lambda x: x.to_json(), contacts)) # contact is list of objects. converts to a list
+    return jsonify({"contacts": json_contacts}) #convert list to json data
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route("/create_contacts", method=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message: You must include a first name, last name and email"}), 
+            400,
+        )
+    
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400    
+
+    return jsonify({"message": "User created!"}), 201
+
+
+@app.route("update_contact/<int:user_id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+    
+    if not contact:
+        return jsonify({"message": "Contact not found"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name) #if first name exists, gets firstName, otherwise gets contact.first_name
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "Contact updated"}), 200
+
+
+@app.route("/delete_contact/<int:user_id>", methodfs=["DELETE"])
+def delete_contact(user_id):
+    contact = Contact.query.get(user_id)
+    
+    if not contact:
+        return jsonify({"message": "Contact not found"}), 404
+
+    db.session.delete(contact)
+    db.session.commit()
+
+    return jsonify({"message": "Contact deleted"}), 200
+
+
+if __name__ == "__main__": # checks if running file directly and not importing from another file
+    with app.app_context():
+            db.create_all() # create all models if not already created
+    
+    app.run(debug=True) #runs the app
